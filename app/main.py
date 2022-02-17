06df56f7167d143a2e5b8e6f34cf4e47214c6673
Y.Hasegawa <hasegawa@zaf.jp>
2022-02-17 17:21:38 +0900

    binary receiver

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import base64
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from io import BytesIO
@@ -8,7 +8,12 @@ from functions import scan_qrcode, parse_inspection
 from config import Config
 
 
-class QRCodeParameter(BaseModel):
+class QRCodeBase64Parameter(BaseModel):
+    base64: str
+    type: str = 'image/jpeg'
+
+
+class QRCodeImageParameter(BaseModel):
     base64: str
     type: str = 'image/jpeg'
 
@@ -23,8 +28,8 @@ app.add_middleware(
 )
 
 
-@app.post('/qrcode')
-async def qrcode(parameter: QRCodeParameter):
+@app.post('/qr-base64')
+async def qrcode(parameter: QRCodeBase64Parameter):
     output = {
         'result': True,
         'error': None,
@@ -34,6 +39,24 @@ async def qrcode(parameter: QRCodeParameter):
         encoded = parameter.base64
         decoded = base64.b64decode(encoded)
         img = Image.open(BytesIO(decoded))
+        lines = scan_qrcode(img)
+        output['parsed'] = parse_inspection(lines)
+    except Exception as ex:
+        output['result'] = False
+        output['error'] = str(ex)
+
+    return output
+
+
+@app.post('/qr-img')
+async def qrcode(data: bytes = Body(...)):
+    output = {
+        'result': True,
+        'error': None,
+    }
+
+    try:
+        img = Image.open(BytesIO(data))
         lines = scan_qrcode(img)
         output['parsed'] = parse_inspection(lines)
     except Exception as ex:
