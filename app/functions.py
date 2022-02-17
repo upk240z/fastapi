3a48a3454730aefa57c6534b209dbcd8b138627e
Y.Hasegawa <hasegawa@zaf.jp>
2022-02-17 19:48:53 +0900

    changed column names

--- app/functions.py
+++ app/functions.py
@@ -16,9 +16,6 @@ def scan_qrcode(img: Image):
     for result in sorted(results, key=lambda x: x.rect.left):
         codes.append(result.data.decode('utf-8'))
 
-    if len(codes) > 0:
-        os.remove(img_file)
-
     return codes
 
 
@@ -44,8 +41,8 @@ def parse_inspection(lines: List[str]):
                 parsed.update({
                     'kana': result.group(1),
                     'rui': result.group(2),
-                    'inspection_fin_date': '20' + ymd[0:2] + '-' + ymd[2:4] + '-' + ymd[4:],
-                    'first_month': '20' + ym[0:2] + '-' + ym[2:]
+                    'inspection-fin-date': '20' + ymd[0:2] + '-' + ymd[2:4] + '-' + ymd[4:],
+                    'first-month': '20' + ym[0:2] + '-' + ym[2:]
                 })
                 continue
         if not matched2:
@@ -53,10 +50,10 @@ def parse_inspection(lines: List[str]):
             if result:
                 matched2 = True
                 parsed['plate'] = {
-                    'riku': result.group(1).replace(' ', ''),
-                    'code': result.group(2),
+                    'area': result.group(1).replace(' ', ''),
+                    'class': result.group(2),
                     'hira': result.group(3),
-                    'no': result.group(4).replace(' ', '0'),
+                    'number': result.group(4).replace(' ', '0'),
                 }
 
     return parsed
